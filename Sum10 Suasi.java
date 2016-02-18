5f9cde187a45cf0b8a079fe9b55003f991886935
c <cis27@scarletmail.rutgers.edu>
2016-02-18 13:38:11 -0500

    sumOf10Ints java

---
+++ Sum10 Suasi.java
@@ -0,0 +1,11 @@
+public class Sum10 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1+2+3+4+5+6+7+8+9+10);
+	}
+
+}
---
+++ Sum10Reciprocals Suasi.java
@@ -0,0 +1,16 @@
+public class FaceMaker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println
+		(" ///////" + //hair
+				"\n | o o |" + //temples + eyes
+				"\n(|  ^  |)" + //ears + nose
+				"\n | [_] |" + //cheeks + mouth
+				"\n -------"); //chin
+	}
+
+}
