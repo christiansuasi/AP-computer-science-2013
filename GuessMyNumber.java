4df5a39973bf007a05ec9ff9351552592caa1aeb
c <cis27@scarletmail.rutgers.edu>
2016-02-28 23:39:44 -0500

    guessmynumber

---
+++ GuessMyNumber.java
@@ -0,0 +1,40 @@
+
+public class GuessMyNumber {
+	public static void main (String[] args){
+		
+		System.out.println("Enter value to guess:");
+		int guess = IO.readInt();
+		
+		int max = 100;
+		int min = 1;
+		int count =0;
+		if (guess< (100-guess)/2){
+			
+		}
+		
+		/*
+		if (guess==50){
+			count++;
+			System.out.println("guess count:" + count);
+		while (guess != )	
+		}else if(guess>50){
+		else if (guess>75){
+		else if (guess>88){
+		else if (guess )
+		}
+		}
+		}
+		else if(guess >75{)
+			
+		}else if (guess >=13){
+		}else if (guess >=7){
+		}else if (guess >=4){
+		}else if (guess >=2){
+		}else if (guess >=1){
+		
+		}
+		
+		*/
+		
+	}
+}
